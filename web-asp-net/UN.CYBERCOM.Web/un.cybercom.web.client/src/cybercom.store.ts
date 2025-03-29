fba99767e6bc63fa83760c68e3312c467f9027f3
Jason L. Lind <lind@yahoo.com>
2025-03-28 23:40:09 -0500

    introducing MobX

---
+++ web-asp-net/UN.CYBERCOM.Web/un.cybercom.web.client/src/cybercom.store.ts
@@ -0,0 +1,192 @@
+import { makeAutoObservable, runInAction } from 'mobx';
+import { ethers } from 'ethers';
+import {
+    CybercomDAO__factory,
+    CybercomDAO,
+    CouncilManager__factory,
+    ProposalStorageManager__factory,
+    Voting__factory,
+    MembershipRemovalManager__factory,
+    MembershipManager__factory,
+    VotingParametersManager__factory
+} from './typechain';
+import { MembershipManagement } from './typechain/contracts/CybercomDAO';
+
+const subscriptionId = BigInt(import.meta.env.VITE_CHAINLINK_VRF_SUBSCRIPTION_ID);
+let initialCybercomContract = import.meta.env.VITE_CYBERCOM_DAO_CONTRACT;
+export class ContractAddresses {
+    daoAddress: string | undefined = undefined;
+    votingAddress: string | undefined = undefined;
+    councilManagementAddress: string | undefined = undefined;
+    proposalStorageAddress: string | undefined = undefined;
+    membershipRemovalAddress: string | undefined = undefined;
+    membershipManagerAddress: string | undefined = undefined;
+    votingParametersManagerAddress: string | undefined = undefined;
+    constructor() {
+        makeAutoObservable(this);
+    }
+    updateObj(obj: MembershipManagement.ContractAddressesStructOutput) {
+        runInAction(() => {
+            this.daoAddress = obj.daoAddress;
+            this.votingAddress = obj.votingAddress;
+            this.councilManagementAddress = obj.councilManagementAddress;
+            this.proposalStorageAddress = obj.proposalStorageAddress;
+            this.membershipManagerAddress = obj.membershipManagerAddress;
+            this.membershipRemovalAddress = obj.membershipRemovalAddress;
+            this.votingParametersManagerAddress = obj.votingParametersManagerAddress;
+        });
+    }
+}
+export class CybercomStore {
+    deploying: boolean = false;
+    connecting: boolean = false;
+    isConnected: boolean = false;
+    isLoading: boolean = false;
+    contract: CybercomDAO | undefined = undefined;
+    contractAddresses: ContractAddresses = new ContractAddresses();
+    provider: ethers.BrowserProvider | undefined = undefined;
+    signer: ethers.JsonRpcSigner | undefined = undefined;
+    activity: string = '';
+    cybercomContract: string | undefined = initialCybercomContract;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    async deployContract(subscription_id: ethers.BigNumberish): Promise<string | undefined> {
+        if (!this.provider || !this.signer) return;
+        this.activity = 'Deploying main contract';
+        const factory = new CybercomDAO__factory(this.signer);
+        const contract = await factory.deploy(subscription_id);
+        await contract.waitForDeployment();
+        const code = await contract.getDeployedCode();
+        if (!code) return;
+
+        this.activity = 'Deploying Council Manager Contract';
+        const address = await contract.getAddress();
+        const councilManagerFactory = new CouncilManager__factory(this.signer);
+        const ct = await councilManagerFactory.deploy(address);
+        await ct.waitForDeployment();
+        const ctCode = await ct.getDeployedCode();
+        if (!ctCode) return;
+
+        this.activity = 'Deploying Proposal Storage Contract';
+        const ctAddress = await ct.getAddress();
+        const propStorContact = new ProposalStorageManager__factory(this.signer);
+        const pCt = await propStorContact.deploy(address);
+        await pCt.waitForDeployment();
+        const pCtCode = await pCt.getDeployedCode();
+        if (!pCtCode) return;
+
+        this.activity = 'Deploying Voting Contract';
+        const pCtAddress = await pCt.getAddress();
+        const vsContract = new Voting__factory(this.signer);
+        const vs = await vsContract.deploy(subscriptionId, address, ctAddress);
+        await vs.waitForDeployment();
+        const vsCode = await vs.getDeployedCode();
+        if (!vsCode) return;
+
+        this.activity = 'Deploying Membership Removal Contract';
+        const vsAddress = await vs.getAddress();
+        const memRemovalFactory = new MembershipRemovalManager__factory(this.signer);
+        const memRemoval = await memRemovalFactory.deploy(vsAddress, ctAddress, pCtAddress, address);
+        await memRemoval.waitForDeployment();
+        const memRemovalCode = await memRemoval.getDeployedCode();
+        if (!memRemovalCode) return;
+
+        this.activity = 'Deploying Membership Management Contract';
+        const memRemovalAddress = await memRemoval.getAddress();
+        const memManContractFactory = new MembershipManager__factory(this.signer);
+        const memMan = await memManContractFactory.deploy(vsAddress, ctAddress, pCtAddress, address);
+        await memMan.waitForDeployment();
+        const memManCode = await memMan.getDeployedCode();
+        if (!memManCode) return;
+
+        this.activity = 'Deploying Voting Parameter Contract';
+        const memManAddress = await memMan.getAddress();
+        const votParmFactory = new VotingParametersManager__factory(this.signer);
+        const votParm = await votParmFactory.deploy(vsAddress, ctAddress, pCtAddress, address);
+        await votParm.waitForDeployment();
+        const votParmCode = await votParm.getDeployedCode();
+        if (!votParmCode) return;
+
+        this.activity = 'Initializing Cybercom Contract';
+        const votParAddress = await votParm.getAddress();
+        const initResp = await contract.initialize({
+            daoAddress: address,
+            votingAddress: vsAddress,
+            votingParametersManagerAddress: votParAddress,
+            proposalStorageAddress: pCtAddress,
+            membershipManagerAddress: memManAddress,
+            membershipRemovalAddress: memRemovalAddress,
+            councilManagementAddress: ctAddress,
+        });
+        await initResp.wait();
+        const closeResp = await contract.closeInitialization();
+        this.activity = 'Finalizing Cybercom Contract';
+        await closeResp.wait();
+        console.log('Contract deployed at:', address);
+        return address;
+    }
+
+    async handleSetupProvider() {
+        try {
+            this.connecting = true;
+            if (window.ethereum) {
+                const prov = new ethers.BrowserProvider(window.ethereum);
+                await prov.send("eth_requestAccounts", []);
+                const sig = await prov.getSigner(0);
+                runInAction(() => {
+                    this.provider = prov;
+                    this.signer = sig;
+                });
+                try {
+                    await window.ethereum.request({
+                        method: 'wallet_switchEthereumChain',
+                        params: [{ chainId: '0xaa36a7' }],
+                    });
+                } catch (switchError) {
+                    console.error("Error switching to the Sepolia network", switchError);
+                }
+                runInAction(() => {
+                    this.isConnected = !!sig;
+                });
+            }
+        } finally {
+            runInAction(() => {
+                this.connecting = false;
+            });
+        }
+    }
+
+    async handleDeployContract() {
+        try {
+            this.deploying = true;
+            const deployedAddress = await this.deployContract(subscriptionId);
+            runInAction(() => {
+                this.cybercomContract = deployedAddress;
+            });
+        } finally {
+            runInAction(() => {
+                this.deploying = false;
+            });
+        }
+    }
+
+    async handleLoadContract() {
+        try {
+            this.isLoading = true;
+            if (!this.signer || !this.cybercomContract) return;
+            const cybercom = CybercomDAO__factory.connect(this.cybercomContract, this.signer);
+            runInAction(() => {
+                this.contract = cybercom;
+            });
+            const addresses = await cybercom.getContractAddresses();
+            this.contractAddresses.updateObj(addresses);
+        } finally {
+            runInAction(() => {
+                this.isLoading = false;
+            });
+        }
+    }
+}
--- web-asp-net/UN.CYBERCOM.Web/un.cybercom.web.client/src/cybercom.tsx
+++ web-asp-net/UN.CYBERCOM.Web/un.cybercom.web.client/src/cybercom.tsx
@@ -1,194 +1,55 @@
-import { useState, useEffect } from 'react';
-import { ethers } from 'ethers';
-import { CybercomDAO__factory, CybercomDAO, CouncilManager__factory, ProposalStorageManager__factory, Voting__factory, MembershipRemovalManager__factory, MembershipManager__factory, VotingParametersManager__factory } from './typechain';
-import { MembershipManagement } from './typechain/contracts/CybercomDAO'
+import React from 'react';
+import { CybercomStore } from './cybercom.store.ts'
+import { observer } from 'mobx-react-lite';
 
-const subscriptionId = BigInt(import.meta.env.VITE_CHAINLINK_VRF_SUBSCRIPTION_ID);
-let cybercomContract = import.meta.env.VITE_CYBERCOM_DAO_CONTRACT;
-function Cybercom() {
+const cybercomStore = new CybercomStore();
 
-    const [deploying, setDeploying] = useState<boolean>(false);
-    const [connecting, setConnecting] = useState<boolean>(false);
-    const [isConnected, setIsConnected] = useState<boolean>(false);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [contract, setContract] = useState<CybercomDAO | undefined>();
-    const [contractAddresses, setContractAddresses] = useState<MembershipManagement.ContractAddressesStructOutput | undefined>();
-    const [provider, setProvider] = useState<ethers.BrowserProvider | undefined>();
-    const [signer, setSigner] = useState<ethers.JsonRpcSigner | undefined>();
-    const [activity, setActivity] = useState<string | undefined>();
-    const deployContract = async (subscription_id: ethers.BigNumberish): Promise<string | undefined> => {
-        if (!provider || !signer)
-            return;
-        setActivity('Deploying main contract');
-        const factory = new CybercomDAO__factory(signer);
-        const contract = await factory.deploy(subscription_id);
-        await contract.waitForDeployment();
-        const code = await contract.getDeployedCode();
-        if (!code)
-            return;
-        setActivity('Deploying Council Manager Contract');
-        const address = await contract.getAddress();
-        const councilManagerFactory = new CouncilManager__factory(signer);
-        const ct = await councilManagerFactory.deploy(address);
-        await ct.waitForDeployment();
-        const ctCode = await ct.getDeployedCode();
-        if (!ctCode)
-            return;
-        setActivity('Deploying Proposal Storage Contract ');
-        const ctAddress = await ct.getAddress();
-        const propStorContact = new ProposalStorageManager__factory(signer);
-        const pCt = await propStorContact.deploy(address);
-        await pCt.waitForDeployment();
-        const pCtCode = await pCt.getDeployedCode();
-        if (!pCtCode)
-            return;
-        setActivity('Deploying Voting Contract');
-        const pCtAddress = await pCt.getAddress();
-        const vsContract = new Voting__factory(signer);
-        const vs = await vsContract.deploy(subscriptionId, address, ctAddress);
-        await vs.waitForDeployment();
-        const vsCode = await vs.getDeployedCode();
-        if (!vsCode)
-            return;
-        setActivity('Deploying Membership Removal Contract');
-        const vsAddress = await vs.getAddress();
-        const memRemovalFactory = new MembershipRemovalManager__factory(signer);
-        const memRemoval = await memRemovalFactory.deploy(vsAddress, ctAddress, pCtAddress, address);
-        await memRemoval.waitForDeployment();
-        const memRemovalCode = await memRemoval.getDeployedCode();
-        if (!memRemovalCode)
-            return;
-        setActivity('Deploying Membership Management Contract');
-        const memRemovalAddress = await memRemoval.getAddress();
-        const memManContractFactory = new MembershipManager__factory(signer);
-        const memMan = await memManContractFactory.deploy(vsAddress, ctAddress, pCtAddress, address);
-        await memMan.waitForDeployment();
-        const memManCode = await memMan.getDeployedCode();
+const Cybercom = observer(() => {
+    const store = cybercomStore;
 
-        if (!memManCode)
-            return;
-        setActivity('Deploying Voting Parameter Contract');
-
-        const memManAddress = await memMan.getAddress();
-        const votParmFactory = new VotingParametersManager__factory(signer);
-        const votParm = await votParmFactory.deploy(vsAddress, ctAddress, pCtAddress, address);
-        await votParm.waitForDeployment();
-        const votParmCode = await votParm.getDeployedCode();
-        if (!votParmCode)
-            return;
-        setActivity('Initializing Cybercom Contract');
-        const votParAddress = await votParm.getAddress();
-        const initResp = await contract.initialize({
-            daoAddress: address,
-            votingAddress: vsAddress,
-            votingParametersManagerAddress: votParAddress,
-            proposalStorageAddress: pCtAddress,
-            membershipManagerAddress: memManAddress,
-            membershipRemovalAddress: memRemovalAddress,
-            councilManagementAddress: ctAddress
-        });;
-        await initResp.wait();
-        var closeResp = await contract.closeInitialization();
-        setActivity('Finalizing Cybercom Contract');
-        await closeResp.wait();
-        console.log('Contract deployed at:', address);
-        return address;
-    };
-    const handleSetupProvider = async () => {
-        try {
-            setConnecting(true);
-            if (window.ethereum) {
-                // Use the BrowserProvider from MetaMask for a signer-enabled provider
-                const prov = new ethers.BrowserProvider(window.ethereum);
-                await prov.send("eth_requestAccounts", []);
-                const sig = await prov.getSigner(0);
-
-                // Set the state after obtaining values
-                setProvider(prov);
-                setSigner(sig);
-                
-                try {
-                    await window.ethereum.request({
-                        method: 'wallet_switchEthereumChain',
-                        params: [{ chainId: '0xaa36a7' }],
-                    });
-                } catch (switchError) {
-                    console.error("Error switching to the Sepolia network", switchError);
-                    // Optionally, you can add code to prompt the user to add the Sepolia network if it isn't available.
-                }
-                setIsConnected(sig !== undefined);
-            }
-        }
-        finally {
-            setConnecting(false);
-        }
-    };
-    const handleDeployContract = async () => {
-        try {
-            setDeploying(true);
-            cybercomContract = await deployContract(subscriptionId);
-        }
-        finally {
-            setDeploying(false);
-        }
-    };
-    const handleLoadContract = async () => {
-        try {
-            setIsLoading(true);
-            const cybercom = CybercomDAO__factory.connect(cybercomContract, signer);
-            if (cybercom !== undefined) {
-                setContract(cybercom);
-                setContractAddresses(await cybercom.getContractAddresses());
-
-            }
-            else {
-                setContract(undefined);
-                setContractAddresses(undefined);
-            }
-        }
-        finally{
-            setIsLoading(false);
-        }
-    };
     return (
         <>
             <title>UNofficial CYBERCOM Portal</title>
             <div>
-                {isConnected && signer && (
+                {store.isConnected && store.signer ? (
                     <div>
                         <h2>Connected to the blockchain!</h2>
-                        <p>Wallet Address : {signer.address}</p>
-                        {cybercomContract && (
+                        <p>Wallet Address: {store.signer.address}</p>
+                        {store.cybercomContract ? (
                             <div>
-                                <p>Contract Address: {cybercomContract}</p>
-                                <button onClick={handleLoadContract} disabled={isLoading}>{isLoading ? 'Loading contract...' : 'Load Contract'}</button>
-                                {contract && contractAddresses && (
+                                <p>Contract Address: {store.cybercomContract}</p>
+                                <button onClick={() => store.handleLoadContract()} disabled={store.isLoading}>
+                                    {store.isLoading ? 'Loading contract...' : 'Load Contract'}
+                                </button>
+                                {store.contract && store.contractAddresses && (
                                     <div>
-                                        <p>Voting: {contractAddresses.votingAddress}</p>
-                                        <p>Voting Parameters: {contractAddresses.votingParametersManagerAddress}</p>
-                                        <p>Council Management: {contractAddresses.councilManagementAddress}</p>
-                                        <p>Membership Manager: {contractAddresses.membershipManagerAddress}</p>
-                                        <p>Membership Removal: {contractAddresses.membershipRemovalAddress}</p>
+                                        <p>Voting: {store.contractAddresses.votingAddress}</p>
+                                        <p>Voting Parameters: {store.contractAddresses.votingParametersManagerAddress}</p>
+                                        <p>Council Management: {store.contractAddresses.councilManagementAddress}</p>
+                                        <p>Membership Manager: {store.contractAddresses.membershipManagerAddress}</p>
+                                        <p>Membership Removal: {store.contractAddresses.membershipRemovalAddress}</p>
                                     </div>
                                 )}
                             </div>
-                        )}
-                        {cybercomContract === undefined && (
+                        ) : (
                             <div>
-                                <button onClick={handleDeployContract} disabled={deploying}>{deploying ? 'Deploying Contract...' : 'Deploy Contract'}</button>
-                                <p>{activity ? activity : ''}</p>
+                                <button onClick={() => store.handleDeployContract()} disabled={store.deploying}>
+                                    {store.deploying ? 'Deploying Contract...' : 'Deploy Contract'}
+                                </button>
+                                <p>{store.activity}</p>
                             </div>
                         )}
                     </div>
-                )}
-                {!isConnected && (
+                ) : (
                     <div>
-                        <button onClick={handleSetupProvider} disabled={connecting}>{connecting ? 'Connecting Wallet...' : 'Connect Wallet'}</button>
+                        <button onClick={() => store.handleSetupProvider()} disabled={store.connecting}>
+                            {store.connecting ? 'Connecting Wallet...' : 'Connect Wallet'}
+                        </button>
                     </div>
                 )}
             </div>
         </>
     );
-}
+});
 
 export default Cybercom;
